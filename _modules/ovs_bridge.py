1845c5e00e2009943a4ba1db06c2617aa752487e
florian.ermisch@fokus.fraunhofer.de <florian.ermisch@fokus.fraunhofer.de>
2014-07-29 16:26:04 +0200

    basic functions like add/delete/exists for OVS bridges

--- _modules/ovs_bridge.py
+++ _modules/ovs_bridge.py
@@ -25,7 +25,64 @@ def test():
     '''
     Test ovs-bridge module.
     '''
-    return true
+    retcode = __salt__['cmd.retcode']('ovs-vsctl show')
+    if retcode == 0:
+        return True
+    else:
+        return False
+
+def add(br):
+    '''
+    Add a OVS bridge.
+    '''
+    # Make sure we don't create None bridges
+    retcode = __salt__['cmd.retcode']('ovs-vsctl add-br {0}'.format(str(br)))
+    if retcode == 0:
+        return True
+    else:
+        return False
+
+def addif(br, iface):
+    '''
+    Add interface as port on given bridge.
+    
+    .. code-block:: bash
+    
+    salt '*' ovs_bridge.addif ovs-br0 eth0
+    '''
+    retcode = __salt__['cmd.retcode'](
+            'ovs-vsctl add-port {0} {1}'.format(str(br),str(iface))
+            )
+    if retcode == 0:
+        return True
+    else:
+        return False
+
+def delete(br):
+    '''
+    Delete given OVS bridge.
+    '''
+    # Managed to create a None bridge, forcing str() to
+    # get rid of those:
+    retcode = __salt__['cmd.retcode']('ovs-vsctl del-br {0}'.format(str(br)))
+    if retcode == 0 and not exists(str(br)):
+        return True
+    else:
+        return False
+
+def exists(br):
+    '''
+    Check if given OVS bridge exists.
+    '''
+    retcode = __salt__['cmd.retcode']('ovs-vsctl br-exists {0}'.format(br))
+    if retcode == 0:
+        return True
+    # From `ovs-vsctl --help`:
+    #     br-exists BRIDGE            exit 2 if BRIDGE does not exist
+    elif retcode == 2:
+        return False
+    else:
+        raise ValueError
 
 def show():
     '''
